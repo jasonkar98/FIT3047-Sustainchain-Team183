294e6223989fef73bce181e2fc9f5d187aa0774e
Naveen Sellathurai <nsel0009@student.monash.edu>
2026-03-29 23:26:19 +1100

    feat: redirect users back to the landing page if they attempt to access edit, delete, view or index pages for enquiries - Commented out code for the initial directing for when it will be needed for admin accounts

--- src/Controller/EnquiriesController.php
+++ src/Controller/EnquiriesController.php
@@ -24,10 +24,11 @@ class EnquiriesController extends AppController
      */
     public function index()
     {
-        $query = $this->Enquiries->find();
-        $enquiries = $this->paginate($query);
-
-        $this->set(compact('enquiries'));
+        return $this->redirect(['controller' => 'Pages', 'action' => 'landingPage']);
+//        $query = $this->Enquiries->find();
+//        $enquiries = $this->paginate($query);
+//
+//        $this->set(compact('enquiries'));
     }
 
     /**
@@ -39,8 +40,9 @@ class EnquiriesController extends AppController
      */
     public function view($id = null)
     {
-        $enquiry = $this->Enquiries->get($id, contain: []);
-        $this->set(compact('enquiry'));
+        return $this->redirect(['controller' => 'Pages', 'action' => 'landingPage']);
+//        $enquiry = $this->Enquiries->get($id, contain: []);
+//        $this->set(compact('enquiry'));
     }
 
     /**
@@ -74,12 +76,8 @@ class EnquiriesController extends AppController
             if ($this->Enquiries->save($enquiry)) {
                 $this->Flash->success(__('The enquiry has been saved.'));
 
-//                return $this->redirect(['action' => 'index']);
-
-                $fallbackLocation = ['controller' => 'Pages', 'action' => 'landingPage'];
-
                 // and redirect user to the location they're trying to access
-                return $this->redirect($fallbackLocation);
+                return $this->redirect(['controller' => 'Pages', 'action' => 'landingPage']);
 
             }
             $this->Flash->error(__('The enquiry could not be saved. Please, try again.'));
@@ -96,17 +94,18 @@ class EnquiriesController extends AppController
      */
     public function edit($id = null)
     {
-        $enquiry = $this->Enquiries->get($id, contain: []);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $enquiry = $this->Enquiries->patchEntity($enquiry, $this->request->getData());
-            if ($this->Enquiries->save($enquiry)) {
-                $this->Flash->success(__('The enquiry has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The enquiry could not be saved. Please, try again.'));
-        }
-        $this->set(compact('enquiry'));
+        return $this->redirect(['controller' => 'Pages', 'action' => 'landingPage']);
+//        $enquiry = $this->Enquiries->get($id, contain: []);
+//        if ($this->request->is(['patch', 'post', 'put'])) {
+//            $enquiry = $this->Enquiries->patchEntity($enquiry, $this->request->getData());
+//            if ($this->Enquiries->save($enquiry)) {
+//                $this->Flash->success(__('The enquiry has been saved.'));
+//
+//                return $this->redirect(['action' => 'index']);
+//            }
+//            $this->Flash->error(__('The enquiry could not be saved. Please, try again.'));
+//        }
+//        $this->set(compact('enquiry'));
     }
 
     /**
@@ -118,14 +117,15 @@ class EnquiriesController extends AppController
      */
     public function delete($id = null)
     {
-        $this->request->allowMethod(['post', 'delete']);
-        $enquiry = $this->Enquiries->get($id);
-        if ($this->Enquiries->delete($enquiry)) {
-            $this->Flash->success(__('The enquiry has been deleted.'));
-        } else {
-            $this->Flash->error(__('The enquiry could not be deleted. Please, try again.'));
-        }
-
-        return $this->redirect(['action' => 'index']);
+        return $this->redirect(['controller' => 'Pages', 'action' => 'landingPage']);
+//        $this->request->allowMethod(['post', 'delete']);
+//        $enquiry = $this->Enquiries->get($id);
+//        if ($this->Enquiries->delete($enquiry)) {
+//            $this->Flash->success(__('The enquiry has been deleted.'));
+//        } else {
+//            $this->Flash->error(__('The enquiry could not be deleted. Please, try again.'));
+//        }
+//
+//        return $this->redirect(['action' => 'index']);
     }
 }
