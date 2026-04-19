0cf1959544740ea0ef7b440ca5a7587e0af3ec12
Naveen Sellathurai <nsel0009@student.monash.edu>
2026-04-19 17:54:18 +1000

    feat: Implemented the edit page - the frontend now looks similar to the frontend of the add page - backened controls the current tags when the edit page is opened, as well as the current image, and ensures that the current image isn't changed if the file input is left empty

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -80,10 +80,7 @@ class ProductsController extends AppController
             }
             $this->Flash->error(__('The product could not be saved. Please, try again.'));
         }
-        // $tags = $this->Products->Tags->find('list', [
-        //     'keyField' => 'id',
-        //     'valueField' => 'name'
-        // ]);
+        
         $filtertags = $this->Products->Filtertags->find('list', limit: 200)->all();
         $this->set(compact('product', 'filtertags'));
     }
@@ -97,17 +94,51 @@ class ProductsController extends AppController
      */
     public function edit($id = null)
     {
-        $product = $this->Products->get($id, contain: []);
+        $product = $this->Products->get($id, contain: ['Filtertags']);
         if ($this->request->is(['patch', 'post', 'put'])) {
-            $product = $this->Products->patchEntity($product, $this->request->getData());
+
+            $data = $this->request->getData();
+
+            $current_image = $product['image_url'];
+
+            // Getting the User ID
+            $user = $this->Authentication->getIdentity();
+            $userId = $user->getIdentifier();
+            $data['user_id'] = $userId;
+
+            // Adding the Image
+            $image = $data['image_url'];
+            $image_name = $image->getClientFilename();
+
+            if (empty($image_name)) {
+                $data['image_url'] = $current_image;
+            } else {
+
+                // Target directory - ensure this folder exists/has permissions
+                $targetPath = WWW_ROOT . '/img/products/' . $image_name;
+
+                // Store the image name in the database
+                $data['image_url'] = $image_name; 
+                
+                if (!empty($image_name)) {
+                    // Move file to webroot folder
+                    $image->moveTo($targetPath);
+                }
+
+            }
+            
+            $product = $this->Products->patchEntity($product, $data);
+            
             if ($this->Products->save($product)) {
                 $this->Flash->success(__('The product has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
+                return $this->redirect(['controller' => 'Pages', 'action' => 'landingPage']);
             }
             $this->Flash->error(__('The product could not be saved. Please, try again.'));
         }
-        $this->set(compact('product'));
+
+        $filtertags = $this->Products->Filtertags->find('list', limit: 200)->all();
+        $this->set(compact('product', 'filtertags'));
     }
 
     /**
