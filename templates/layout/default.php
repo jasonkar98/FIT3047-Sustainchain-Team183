af12ef061326cb5d18d931da6bf73c74c53809b9
Naveen Sellathurai <nsel0009@student.monash.edu>
2026-05-09 22:48:30 +1000

    fix: User restrictions now prevent crashing if the user is not logged in at all

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -612,8 +612,10 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
         <ul class="nav-links">
             <li><?= $this->Html->link('Marketplace', ['prefix' => false, 'controller' => 'Products', 'action' => 'index']) ?></li>
             <?php $identity = $this->request->getAttribute('identity'); ?>
-            <?php if ($identity->get('role') !== 'admin'): ?>
-                <li><?= $this->Html->link('Contact Us', ['prefix' => false, 'controller' => 'Enquiries', 'action' => 'add']) ?></li>
+            <?php if ($identity): ?>
+                <?php if ($identity->get('role') !== 'admin'): ?>
+                    <li><?= $this->Html->link('Contact Us', ['prefix' => false, 'controller' => 'Enquiries', 'action' => 'add']) ?></li>
+                <?php endif; ?>
             <?php endif; ?>
         </ul>
     </div>
